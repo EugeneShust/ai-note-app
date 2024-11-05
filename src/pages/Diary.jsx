1f8d54fdbe7eca0fee97a99fe3ceae3e8b0c47f9
Yevhen <eugene.shust@gmail.com>
2024-11-05 16:50:15 +0100

    first commit

---
+++ src/pages/Diary.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { CreateEntry, MoodAIAnalysis, EntriesList } from '@/components/Diary';
+import { toast } from 'react-toastify';
+
+const Diary = () => {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(`${import.meta.env.VITE_NOTES_API}/entries`);
+        setEntries(data);
+      } catch (error) {
+        toast.error(error.message);
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+      <EntriesList entries={entries} />
+      <CreateEntry setEntries={setEntries} />
+      <MoodAIAnalysis entries={entries} />
+    </>
+  );
+};
+
+export default Diary;
